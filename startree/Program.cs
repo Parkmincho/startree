44e0d5c1057a2d8ab1114dbaa7d852de467c334c
Mincho Park <s2018s28@e-mirim.hs.kr>
2020-05-12 18:52:19 +0900

    별찍기

--- startree/Program.cs
+++ startree/Program.cs
@@ -10,6 +10,19 @@ namespace startree
     {
         static void Main(string[] args)
         {
+            int n=8;
+            for(int i = 0; i < n; i++)
+            {
+                for(int j = n - i - 1; j > 0; j--)
+                {
+                    Console.Write(" ");
+                }
+                for(int k = 0; k < (2 * i) + 1; k++)
+                {
+                    Console.Write("*");
+                }
+                Console.WriteLine();
+            }
         }
     }
 }
